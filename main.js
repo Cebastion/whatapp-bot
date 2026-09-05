b1869024c0775f39bec9425af56f8cc919963375
d.kosenko <d.kosenko@cleack.com>
2026-09-05 16:57:14 +0300

    added express

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import {bot} from "./bot.js";
+import  express from 'express';
+
+const app = express();
+app.get("/", (req, res) => res.send("OK"));
+app.get("/health", (req, res) => res.send("OK"));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Health check server started");
+});
 
 const sock = await bot.connect()
 
